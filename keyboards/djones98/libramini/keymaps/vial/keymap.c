781b96a080f4abb814ab30d04ea65af4b721bbb5
djones98 <jonesdarren1245@gmail.com>
2022-06-22 18:03:13 +0100

    fixed libramini support

--- keyboards/djones98/libramini/keymaps/vial/keymap.c
+++ keyboards/djones98/libramini/keymaps/vial/keymap.c
@@ -1,26 +1,25 @@
 #include QMK_KEYBOARD_H
 #include "libramini.h"
-//#include "print.h"
+#include "libramini/config.h"
 
-enum custom_keycodes {
-    ARROW_KEYS = SAFE_RANGE,
-};
-
-bool    set_arrows = false;
+bool    set_arrows = true;
 int16_t cum_x      = 0;
 int16_t cum_y      = 0;
 int16_t sensor_x   = 0;
 int16_t sensor_y   = 0;
 int16_t cur_factor = 30;
 
+enum custom_keycodes {
+    ARROW_KEYS = USER00,
+};
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	LAYOUT(
 		KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-		KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_SPACE), LT(2,KC_BSPC), ARROW_KEYS, KC_BTN1, KC_BTN2, KC_RSFT,KC_DEL),
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(2), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+		KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_SPACE), KC_BSPC, ARROW_KEYS, KC_BTN1, KC_BTN2, KC_RSFT,KC_DEL),
 
 	LAYOUT(
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
@@ -32,13 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 		KC_TRNS, KC_HOME, KC_PGUP, KC_INS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_END, KC_PGDN, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12),
-
-	LAYOUT(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12)
 
 };
 // clang-format
