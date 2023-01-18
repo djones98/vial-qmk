91b0aedc0fe3b1065cf90412d3b2d13c453ad6bf
djones98 <jonesdarren1245@gmail.com>
2023-01-18 19:53:07 +0000

    yes

--- keyboards/djones98/libramini/config.h
+++ keyboards/djones98/libramini/config.h
@@ -7,7 +7,6 @@
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Daz
 #define PRODUCT Daz Libra Mini
-#define DESCRIPTION Daz Libra Mini
 
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
@@ -16,7 +15,6 @@
     { E6, B4, B5, B2 }
 #define MATRIX_COL_PINS \
     { B3, B1, F7, F6, F5, F4, C6, D4, D0, D1, D2, D3 }
-#define UNUSED_PINS
 
 #define ANALOG_JOYSTICK_X_AXIS_PIN D7
 #define ANALOG_JOYSTICK_Y_AXIS_PIN B6
@@ -24,5 +22,6 @@
 #define ANALOG_JOYSTICK_SPEED_REGULATOR 5
 
 #define DIODE_DIRECTION ROW2COL
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 3
 #define FORCE_NKRO
+#define USB_POLLING_INTERVAL_MS 1
