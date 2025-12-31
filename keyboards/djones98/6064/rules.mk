9727e465f543dafaf6fee8a42f2aea9591defb22
Darren <jonesdarren1245@gmail.com>
2025-12-31 19:20:06 +0000

    add 6064

---
+++ keyboards/djones98/6064/rules.mk
@@ -0,0 +1,15 @@
+MCU = STM32F401
+BOARD = GENERIC_STM32_F401XC
+BOOTLOADER = tinyuf2
+    # Enable Bootmagic Lite
+ENCODER_ENABLE = no
+UARTENABLE = yes
+
+CONSOLE_ENABLE = no        # Console for debug
+
+RGBLIGHT_ENABLE = no    # Enable keyboard RGB underglow
+WS2812_DRIVER = pwm
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
+
+
